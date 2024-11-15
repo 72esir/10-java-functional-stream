12c5a76ee019b3582a3b6c2dd84153017fb34867
Aleksey Salamatin <asalamatin640@gmail.com>
2024-11-15 22:32:36 +0500

    Salamatin Aleksey Task02

--- task02/src/com/example/task02/Task02Main.java
+++ task02/src/com/example/task02/Task02Main.java
@@ -1,23 +1,30 @@
 package com.example.task02;
 
+import java.util.NoSuchElementException;
 import java.util.stream.IntStream;
 
 public class Task02Main {
 
     public static void main(String[] args) {
 
-        /*
+
         cycleGrayCode(2)
                 .limit(10)
                 .forEach(System.out::println);
-        */
+
 
     }
 
     public static IntStream cycleGrayCode(int n) {
+        if (n < 1 || n >16){
+            throw new IllegalArgumentException("Введено некорректное значение");
+        }
+        IntStream intStream = IntStream
+                .iterate(0, x -> (x + 1) % (int)Math.pow(2, n))
+                .map(x -> x ^ (x >> 1));
 
-        return null; // your implementation here
-
+        return intStream; // your implementation here
     }
+
 
 }
