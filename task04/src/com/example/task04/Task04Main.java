6fed4b659b48817e93e8a2738f54e3b237fcd7e5
Aleksey Salamatin <asalamatin640@gmail.com>
2024-11-24 00:22:21 +0500

    Salamatin Aleksey Task04

--- task04/src/com/example/task04/Task04Main.java
+++ task04/src/com/example/task04/Task04Main.java
@@ -1,11 +1,30 @@
 package com.example.task04;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.*;
+import java.util.function.ToLongFunction;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static java.nio.charset.StandardCharsets.UTF_8;
+
 public class Task04Main {
 
     public static void main(String[] args) {
+        Stream<String> stream = new BufferedReader(new InputStreamReader(System.in)).lines();
 
-        // your implementation here
-
+        stream
+                .map(String::toLowerCase)
+                .flatMap(x -> Stream.of(x.split("[^a-zа-яё0-9]")))
+                .filter(x -> !x.isEmpty())
+                .collect(Collectors.groupingBy(n -> n, Collectors.counting()))
+                .entrySet()
+                .stream()
+                .sorted(Comparator.comparingLong((ToLongFunction<Map.Entry<String, Long>>) Map.Entry::getValue)
+                        .reversed()
+                        .thenComparing(Map.Entry::getKey))
+                .limit(10)
+                .forEach(x -> System.out.print(x.getKey() + '\n'));
     }
-
 }
